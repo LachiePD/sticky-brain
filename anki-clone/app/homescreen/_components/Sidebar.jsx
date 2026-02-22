3269e0c8b9f755feca87c18a99d88ba3c794514c
lachie <lachiepd@gmail.com>
2026-02-22 14:58:54 +1100

    added a subheader to Library, made a css field for it

--- anki-clone/app/homescreen/_components/Sidebar.jsx
+++ anki-clone/app/homescreen/_components/Sidebar.jsx
@@ -1,18 +1,24 @@
 import NavItem from "./NavItem.jsx";
-import { useDeckList } from "../DeckListContext.jsx";
+import Subheader from "@/components/headers/Subheader.jsx";
 
+import { useDeckList } from "../DeckListContext.jsx";
 const SideBar = () => {
   const deckList = useDeckList();
+
+  const renderDecks = () => {
+    return deckList.decks.map((deck) => (
+      <NavItem
+        deckSelectionEvent={deckList.actions.selectDeckById}
+        handleDelete={deckList.actions.removeDeck}
+        key={deck.id}
+        data={deck}
+      />
+    ));
+  };
   return (
     <div className={" bg-secondary"}>
-      {deckList.decks.map((deck) => (
-        <NavItem
-          deckSelectionEvent={deckList.actions.selectDeckById}
-          handleDelete={deckList.actions.removeDeck}
-          key={deck.id}
-          data={deck}
-        />
-      ))}
+      <Subheader>Library</Subheader>
+      {renderDecks()}
     </div>
   );
 };
