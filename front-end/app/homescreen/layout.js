d79b7d8dbb796fce2bb820756d0b329f4853ff56
lachie <lachiepd@gmail.com>
2026-02-25 01:17:03 +1100

    layout: wired the ActiveCardContext

--- front-end/app/homescreen/layout.js
+++ front-end/app/homescreen/layout.js
@@ -1,10 +1,12 @@
 import { DeckListProvider } from "@/features/deck/index.js";
 import { ActiveDeckProvider } from "@/features/deck/index.js";
-
+import { ActiveCardProvider } from "@/features/flashcard/index.js";
 const HomescreenLayout = ({ children }) => {
   return (
     <DeckListProvider>
-      <ActiveDeckProvider>{children}</ActiveDeckProvider>
+      <ActiveDeckProvider>
+        <ActiveCardProvider>{children}</ActiveCardProvider>
+      </ActiveDeckProvider>
     </DeckListProvider>
   );
 };
