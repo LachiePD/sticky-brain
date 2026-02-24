35e6ac8a92de36d33a6fb9a56628abd8a0b6e82b
lachie <lachiepd@gmail.com>
2026-02-24 19:45:55 +1100

    Interface now decides what to render

--- front-end/features/deck/interface/Interface.jsx
+++ front-end/features/deck/interface/Interface.jsx
@@ -2,13 +2,29 @@ import { Card } from "@/components/ui/Card";
 import { useActiveDeck } from "../providers/ActiveDeckProvider";
 import { Creator } from "./Creator";
 import { DeckController } from "../views/DeckController";
+
+import { useFlashcard } from "@/features/flashcard/index";
+
 export const Interface = () => {
   const activeDeck = useActiveDeck();
+  const flashcard = useFlashcard();
+
+  const chooseOutput = () => {
+    if (activeDeck.mode.currentMode === "practice") {
+      return (
+        <>
+          <button onClick={card.toggleRevealed}>Try Again</button>
+          <button onClick={card.nextCard}> Correct! </button>
+        </>
+      );
+    }
+  };
+
   return (
     <Card className={"flex-row  items-end"}>
       {!activeDeck.deckId && <Creator />}
 
-      {activeDeck.deckId && <DeckController />}
+      {activeDeck.deckId && chooseOutput()}
     </Card>
   );
 };
