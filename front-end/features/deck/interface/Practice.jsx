8eb84b0e0d7f46b684691e9123b39003fda48b43
lachie <lachiepd@gmail.com>
2026-02-25 02:05:32 +1100

    made isRevealed work

--- front-end/features/deck/interface/Practice.jsx
+++ front-end/features/deck/interface/Practice.jsx
@@ -2,10 +2,18 @@ import { useActiveCard } from "@/features/flashcard/index";
 
 export const Practice = () => {
   const activeCard = useActiveCard();
-  return (
-    <>
-      <button onClick={activeCard.toggleRevealed}>Try Again</button>
-      <button onClick={activeCard.nextCard}> Correct! </button>
-    </>
-  );
+
+  console.log(activeCard.isRevealed);
+  if (activeCard.isRevealed) {
+    return (
+      <>
+        <button onClick={activeCard.toggleRevealed}>Try Again</button>
+        <button onClick={activeCard.nextCard}> Correct! </button>
+      </>
+    );
+  }
+
+  if (!activeCard.isRevealed) {
+    return <button onClick={activeCard.toggleRevealed}> Show Answer!</button>;
+  }
 };
