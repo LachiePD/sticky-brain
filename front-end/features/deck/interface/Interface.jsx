a56bfb6804757cbbf4ba45eaf34fe4868a339561
lachie <lachiepd@gmail.com>
2026-02-24 17:18:15 +1100

    fixed wiring independency bug

--- front-end/features/deck/interface/Interface.jsx
+++ front-end/features/deck/interface/Interface.jsx
@@ -1,20 +1,16 @@
-import {Card} from '@/components/ui/Card';
-import {useActiveDeck} from '../providers/ActiveDeckProvider';
-import {Creator} from './Creator';
-export const Interface = () =>{
-
-	const activeDeck = useActiveDeck();
-	return (
-
-<Card className={"flex-row  items-end"}>
-          {activeDeck.deckId ? (
-            <DeckController key={activeDeck.deckId} />
-          ) : (
-            <Creator />
-          )}
-        </Card>
-	)
-
-
-
-}
+import { Card } from "@/components/ui/Card";
+import { useActiveDeck } from "../providers/ActiveDeckProvider";
+import { Creator } from "./Creator";
+import { DeckController } from "../views/DeckController";
+export const Interface = () => {
+  const activeDeck = useActiveDeck();
+  return (
+    <Card className={"flex-row  items-end"}>
+      {activeDeck.deckId ? (
+        <DeckController key={activeDeck.deckId} />
+      ) : (
+        <Creator />
+      )}
+    </Card>
+  );
+};
