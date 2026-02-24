dec085c22760d68fab9bbba69078e13db6b2a8cc
lachie <lachiepd@gmail.com>
2026-02-24 20:21:33 +1100

    cleaning up after removal of DeckController

--- front-end/features/deck/index.js
+++ front-end/features/deck/index.js
@@ -1,10 +1,9 @@
 export { DeckListProvider, useDeckList } from "./providers/DeckListContext";
-export {Interface} from './interface/Interface';
-export {Stage} from './Stage';
+export { Interface } from "./interface/Interface";
+export { Stage } from "./Stage";
 export {
   ActiveDeckProvider,
   useActiveDeck,
 } from "./providers/ActiveDeckProvider";
-export { DeckController } from "./views/DeckController";
 
 export { useDeckMode } from "./hooks/useDeckMode";
