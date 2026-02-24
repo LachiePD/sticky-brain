e7f4d9b120d23bb443eb1402ac87ab04a35ebe6e
lachie <lachiepd@gmail.com>
2026-02-24 17:47:42 +1100

    changed Interface to only show Creator whether or not a deck is selected

--- front-end/features/deck/interface/Interface.jsx
+++ front-end/features/deck/interface/Interface.jsx
@@ -1,16 +1,12 @@
 import { Card } from "@/components/ui/Card";
 import { useActiveDeck } from "../providers/ActiveDeckProvider";
 import { Creator } from "./Creator";
-import { DeckController } from "../views/DeckController";
+
 export const Interface = () => {
   const activeDeck = useActiveDeck();
   return (
     <Card className={"flex-row  items-end"}>
-      {activeDeck.deckId ? (
-        <DeckController key={activeDeck.deckId} />
-      ) : (
-        <Creator />
-      )}
+      {!activeDeck.deckId && <Creator />}
     </Card>
   );
 };
