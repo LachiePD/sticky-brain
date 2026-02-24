0817d09f3f9696214e15bb499066812e529f94eb
lachie <lachiepd@gmail.com>
2026-02-24 20:20:02 +1100

    Stage now controls what it renders

--- front-end/features/deck/Stage.jsx
+++ front-end/features/deck/Stage.jsx
@@ -1,18 +1,21 @@
 import { Card } from "@/components/ui/Card";
-import { DeckController } from "./views/DeckController";
 import { useActiveDeck } from "./providers/ActiveDeckProvider";
-
+import { Inspect } from "./views/Inspect";
+import { Practice } from "./views/Practice";
 export const Stage = () => {
   const activeDeck = useActiveDeck();
 
-  if (activeDeck.mode.currentMode === "inspecting") {
-    return <div>this is the inspecting of deck,</div>;
-  }
-  if (activeDeck.mode.currentMode === "practicing") {
-    return <div>this is the practicing of deck,</div>;
-  }
+  const renderMode = () => {
+    switch (mode.currentMode) {
+      case "practicing":
+        return <Practice />;
 
-  if (activeDeck.mode.currentMode === "editing") {
-    return <div>this is the editing of deck,</div>;
-  }
+      case "editing":
+        return <p>"We are Editing"</p>;
+
+      case "inspecting":
+        return <Inspect />;
+    }
+    return <div>{renderMode()}</div>;
+  };
 };
