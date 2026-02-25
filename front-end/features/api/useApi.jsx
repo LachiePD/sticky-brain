5255f195cad0993acec0c1c038e7c540edf0c53d
lachie <lachiepd@gmail.com>
2026-02-26 03:17:22 +1100

    /useApi: implementing createUser

--- front-end/features/api/useApi.jsx
+++ front-end/features/api/useApi.jsx
@@ -3,6 +3,12 @@ import { auth } from "./auth.api.js";
 import { card } from "./card.api.js";
 import { deck } from "./deck.api.js";
 
-export const useApi = ({ route }) => {
+export const useApi = () => {
   const [response, setResponse] = useState(null);
+
+  const createUser = async (credentials) => {
+    auth.createUser(credentials);
+  };
+
+  return { createUser };
 };
