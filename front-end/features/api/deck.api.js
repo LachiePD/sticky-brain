a1cd822a7ef625f195f5a47538258dc1202d5bc9
lachie <lachiepd@gmail.com>
2026-02-26 03:46:14 +1100

    fix:dependency

--- front-end/features/api/deck.api.js
+++ front-end/features/api/deck.api.js
@@ -1,5 +1,4 @@
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-import { MOCK_DECKS } from "../mockData.js";
 export const submitNewDeck = async (deckName) => {
   const response = await fetch(`${apiUrl}/createdeck`, {
     method: "POST",
