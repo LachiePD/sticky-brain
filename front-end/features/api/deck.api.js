18e53521215fc72230425c15fb0d590cd2ed43a6
lachie <lachiepd@gmail.com>
2026-02-27 16:53:57 +1100

    refactor deck.api

--- front-end/features/api/deck.api.js
+++ front-end/features/api/deck.api.js
@@ -1,21 +1,12 @@
+import { request } from "./request";
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-async function request(path, options = {}) {
-  const response = await fetch(`${apiUrl}${path}`, {
-    headers: { "Content-Type": "application/json" },
-    credentials: "include",
-    ...options, // This lets us overwrite or add method/body
-  });
+const getDecks = () => request("/getAllDecks", { method: "GET" });
 
-  const data = await response.json();
-  return { ...data, status: response.status };
-}
-export const getDecks = () => request("/getAllDecks", { method: "GET" });
-
-export const removeDeck = (id) =>
+const removeDeck = (id) =>
   request(`/removeDeck/${encodeURIComponent(id)}`, { method: "DELETE" });
 
-export const createDeck = (payload) =>
+const createDeck = (payload) =>
   request("/createDeck", {
     method: "POST",
     body: JSON.stringify(payload),
