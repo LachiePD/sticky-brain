066b0863fafa34c3131e685750be04d9027d20f1
lachie <lachiepd@gmail.com>
2026-04-23 16:05:46 +1000

    fix: remove invalid max-w-screen class from body

--- front-end/app/layout.js
+++ front-end/app/layout.js
@@ -4,7 +4,7 @@ import { AuthProvider } from "@/providers/index.mjs";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={" flex max-w-screen  bg-primary  "}>
+      <body className={"bg-primary"}>
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
