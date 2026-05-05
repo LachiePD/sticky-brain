5fc0755d397f405f7e3409c7a7e626609fcb8ba6
lachie <lachiepd@gmail.com>
2026-05-05 21:32:03 +1000

    feature: show spinner with backend warmup message while loading

--- front-end/app/login/page.js
+++ front-end/app/login/page.js
@@ -2,21 +2,31 @@
 import { useRouter } from "next/navigation";
 import { Card, LoginForm } from "@/components/index";
 import { useState } from "react";
+import { Spinner } from "@/components/ui/spinner";
+
 const Page = () => {
   const [loading, setLoading] = useState(true);
   const router = useRouter();
   return (
     <main className={"flex w-full min-h-screen items-center justify-center"}>
-      {loading ? (
-        <p>hi</p>
-      ) : (
-        <Card header={"Login"}>
-          <LoginForm />
-          <button onClick={() => router.push("/")}>
-            New here? Create an account
-          </button>
-        </Card>
-      )}
+      <Card className={"flex"} header={"Login"}>
+        {loading ? (
+          <>
+            <Spinner className={"size-8"} />
+            <p>
+              Please note, if you had to wait for the frontend to boot up, then
+              the backend is probably still booting up too, please wait
+            </p>
+          </>
+        ) : (
+          <>
+            <LoginForm />
+            <button onClick={() => router.push("/")}>
+              New here? Create an account
+            </button>
+          </>
+        )}
+      </Card>
     </main>
   );
 };
