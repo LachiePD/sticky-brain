90fe61d058840e61fc6d09dcad9817d64728b402
lachie <lachiepd@gmail.com>
2026-05-05 21:10:47 +1000

    feature: shadcn components dependencies

--- front-end/app/login/page.js
+++ front-end/app/login/page.js
@@ -1,17 +1,22 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { Card, LoginForm } from "@/components/index";
-
+import { useState } from "react";
 const Page = () => {
+  const [loading, setLoading] = useState(true);
   const router = useRouter();
   return (
     <main className={"flex w-full min-h-screen items-center justify-center"}>
-      <Card header={"Login"}>
-        <LoginForm />
-        <button onClick={() => router.push("/")}>
-          New here? Create an account
-        </button>
-      </Card>
+      {loading ? (
+        <p>hi</p>
+      ) : (
+        <Card header={"Login"}>
+          <LoginForm />
+          <button onClick={() => router.push("/")}>
+            New here? Create an account
+          </button>
+        </Card>
+      )}
     </main>
   );
 };
